9c2dde18c206a760be996b84dfd8b9ade11eb1b5
IevgeniiaAbdulina <ievgeniiaabdulina@gmail.com>
2021-03-23 13:04:03 +0100

    Added Background <body> Style depends on screen size

--- src/components/Attribution.js
+++ src/components/Attribution.js
@@ -1,6 +1,6 @@
 const Attribution = () => {
     return ( 
-        <div class="attribution">
+        <div className="attribution">
             Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
             Coded by <a href="https://github.com/IevgeniiaAbdulina/Note-React#screenshot">Ievgeniia Abdulina</a>.
         </div>
