0c0f86a07797ca1aecdf7d7ddff599e336109efc
IevgeniiaAbdulina <ievgeniiaabdulina@gmail.com>
2021-03-23 12:28:33 +0100

    View the optimal layout for the app depending on their device's screen size (added media css rules)

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Note react Application
+# Note React Application
 
 This is a solution to the [Todo app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/todo-app-Su1_KokOW).
 
--- src/components/NotesDetails.js
+++ src/components/NotesDetails.js
@@ -2,6 +2,11 @@ const NotesDetails = () => {
     return ( 
         <div className="notes-details">
             <div className="note-left">Note left</div>
+            <div className="layout-desktop">
+                <div className="notes-all">All</div>
+                <div className="notes-active">Active</div> 
+                <div className="notes-completed">Completed</div> 
+            </div>
             <div className="clear-completed">Clear completed</div>
         </div>
      );
