2419bf8a89a8ee42663b9edc2e0209e6540c9951
IevgeniiaAbdulina <ievgeniiaabdulina@gmail.com>
2021-03-25 17:18:10 +0100

    Added Select Filter CSS Style

--- src/components/NotesFilter.js
+++ src/components/NotesFilter.js
@@ -1,9 +1,17 @@
+import {useState} from "react";
+
 const NotesFilter = () => {
+    const [selectedItem, setSelectedItem] = useState('all');
+
+    const filterNotes = (selected) => {
+        setSelectedItem(selected);
+    }
+    
     return ( 
         <div className="notes-filter container">
-            <div className="notes-all selected">All</div>
-            <div className="notes-active">Active</div> 
-            <div className="notes-completed">Completed</div> 
+            <div onClick={() => filterNotes('all')} className={`notes-all ${selectedItem === 'all' ? 'selected' : ''}`}>All</div>
+            <div onClick={() => filterNotes('active')} className={`notes-active ${selectedItem === 'active' ? 'selected' : ''}`}>Active</div> 
+            <div onClick={() => filterNotes('completed')} className={`notes-completed ${selectedItem === 'completed' ? 'selected' : ''}`}>Completed</div> 
         </div>
      );
 }
