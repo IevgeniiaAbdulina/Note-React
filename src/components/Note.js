cf9bc19193dac9dffe76e2ba5835ad003fdcb5f3
IevgeniiaAbdulina <ievgeniiaabdulina@gmail.com>
2021-04-01 16:26:44 +0200

    Added DRAG and DROP to Reorder Items with every step comments

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,8 +1,17 @@
 
-const Note = ({ note, completeHandler, deleteHandler, colorMode }) => {
+const Note = ({ note, index, completeHandler, deleteHandler, colorMode, onDragStart, onDragOver, onDrop, onDragLeave, classNameDnD }) => {
     
     return ( 
-        <div className={`note ${colorMode} drop-item`} draggable='true' id={note.id}>
+        <div 
+            className={`note ${colorMode} ${classNameDnD}`} 
+
+            draggable='true'  
+            onDragStart={onDragStart}
+            onDragOver={onDragOver}
+            onDrop={onDrop}
+            data-position={index}
+            onDragLeave={onDragLeave}
+            >
             <div className="wrapper">
                 <div onClick={completeHandler}
                     className={`mark ${!note.completed ? 'uncompleted' : 'completed'} ${colorMode}`}
--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -15,90 +15,6 @@ const NoteList = ({colorMode}) => {
         setNotesList(notes);
     }, [notes]);
 
-    // Drag and Drop Start  _______________________________
-
-    useEffect(() => {
-       if(notes.length < 1) {
-        return
-       } else {
-        dragAndDrop();
-       }
-    }, [notes])
-
-    const dragAndDrop = () => {
-        let dragEl;
-
-        const dragStart = (e) => {
-            console.log('dragEl  ', dragEl)
-            console.log('e.target  ', e.target.id)
-             e.target.style.opacity = '0.4';
-
-             dragEl = e.target;
-             e.dataTransfer.effectAllowed = 'move';
-             e.dataTransfer.setData('text/html', e.target.innerHTML);
-        }
-
-        const dragOver = (e) => {
-            if(e.preventDefault()) {
-                e.preventDefault();
-            }
-            e.dataTransfer.dropEffect = 'move';
-            return false;
-        }
-
-        const dragEnter = (e) => {
-            e.target.classList.add('over');
-        }
-
-        const dragLeave = (e) => {
-            e.target.classList.remove('over');
-        }
-
-        const dragDrop = (e) => {
-            console.log('dragEl  ', dragEl)
-            console.log('e.target  ', e.target.id)
-            if(e.stopPropagation()) {
-                e.stopPropagation();  // stop the browser from redirecting
-            }
-
-            if(dragEl !== e.target) {
-                dragEl.innerHTML = e.target.innerHTML;
-                e.target.innerHTML = e.dataTransfer.getData('text/html');
-            }
-            
-
-            return false;
-        }
-
-        
-        const dragEnd =(e) => {
-            e.target.style.opacity = '1';
-
-            items.forEach((item) => {
-                item.classList.remove('over');
-            })
-        }
-
-        let items = document.querySelectorAll('.drop-area .drop-item');
-        if(items === null) {
-            return
-        } else {
-            items.forEach((item) => {
-                console.log("ITEM: ", item)
-                item.addEventListener('dragstart', dragStart, false);
-                item.addEventListener('dragenter', dragEnter, false);
-                item.addEventListener('dragover', dragOver, false);
-                item.addEventListener('dragleave', dragLeave, false);
-                item.addEventListener('drop', dragDrop, false);
-                item.addEventListener('dragend', dragEnd, false);
-            })
-        }
-    }
-    //  DnD End ____________________________________________
-    // ///////////////////////////////////////////////////////////////////////////////////
-
-    // ///////////////////////////////////////////////////////////////////////////////////
-
     useEffect(() => {
         updateScreenSize();
         window.addEventListener('resize', updateScreenSize);
@@ -108,7 +24,7 @@ const NoteList = ({colorMode}) => {
     }, [windowWidth]);
 
     useEffect(() => {
-        getNotesLeft()
+        getNotesLeft();
     }, [notesList, notesLeft])
 
     const updateScreenSize = () => {
@@ -167,24 +83,160 @@ const NoteList = ({colorMode}) => {
         setNotes(notes.filter(item => item.completed === false))
     }
 
+
     
-   
-    return ( 
-        <>
-            <NewNote 
-                handleChange={handleChange} 
-                handleSubmit= {handleSubmit}
-                newNote={newNote}
-                colorMode={colorMode}
-            />
-            <div className={`note-list container ${colorMode} drop-area`}>
-                {notesList.map(note => 
+    /////////////////////////////////////////////////////////////////////////////////////
+    // DRAG and DROP TO REORDER ITEMS ON THE LIST
+    const initialDnDState = {
+        draggedFrom: null,
+        draggedTo: null,
+        isDragging: false,
+        originalOrder: [],
+        updatedOrder: []
+    } 
+
+    const DragToReorderList = () => {
+        // We'll use the initialDndState created above
+        const [dragAndDrop, setDragAndDrop] = useState(initialDnDState);
+
+        const onDragStart = (e) => {
+            // It receives a DragEvent
+            // which inherits properties from
+            // MouseEvent and Event
+            // so we can access the element
+            // through event.currentTarget
+    
+            // Later, we'll save
+            // in a hook variable
+            // the item being dragged
+    
+            console.log('onDragStart');
+    
+            // We'll access the "data-position" attribute
+            // of the current element dragged
+            const initialPosition = Number(e.currentTarget.dataset.position);
+            console.log('initialPosition', initialPosition)
+    
+            setDragAndDrop({
+                 // we spread the previous content
+                // of the hook variable
+                // so we don't override the properties 
+                // not being updated
+                ...dragAndDrop, 
+    
+                draggedFrom: initialPosition, // set the draggedFrom position
+                isDragging: true,
+                originalOrder: notesList // store the current state of "list"
+            });
+
+            console.log("dragAndDrop: ", dragAndDrop)
+
+            // Note: this is only for Firefox.
+            e.dataTransfer.setData("text/html", '');
+    
+        }
+
+        const onDragOver = (e) => {
+            // It also receives a DragEvent.
+            // Later, we'll read the position
+            // of the item from event.currentTarget
+            // and store the updated list state
+    
+            // We need to prevent the default behavior
+            // of this event, in order for the onDrop
+            // event to fire.
+            // It may sound weird, but the default is
+            // to cancel out the drop.
+    
+            e.preventDefault();
+            console.log('onDragOver');
+
+            // Store the content of the original list
+            // in this variable that we'll update
+            let newList = dragAndDrop.originalOrder;
+            console.log('newList: ', newList);
+
+            // index of the item being dragged
+            const draggedFrom = dragAndDrop.draggedFrom;
+            console.log('draggedFrom: ', draggedFrom);
+
+            // index of the drop area being hovered
+            const draggedTo = Number(e.currentTarget.dataset.position);
+            console.log('draggedTo: ', draggedTo);
+
+            // get the element that's at the position of "draggedFrom"
+            const itemDragged = newList[draggedFrom];
+            console.log('itemDragged: ', itemDragged);
+
+            // filter out the item being dragged
+            const remainingItems = newList.filter((item, index) => index !== draggedFrom);
+
+            // update the list
+            newList = [
+                ...remainingItems.slice(0, draggedTo),
+                itemDragged,
+                ...remainingItems.slice(draggedTo)
+            ];
+
+            // check if the targets are different
+            if(draggedTo !== dragAndDrop.draggedTo) {
+                setDragAndDrop({
+                    ...dragAndDrop,
+
+                    // save the updated list state
+                    // we will render this onDrop
+                    updatedOrder: newList,
+                    draggedTo: draggedTo
+                })
+            }
+
+        }
+    
+        const onDrop = (e) => {
+            // Here, we will:
+            // - update the rendered list
+            // - and reset the DnD state
+            console.log('onDrop');
+
+            // we use the updater function
+            // for the "list" hook
+            setNotesList(dragAndDrop.updatedOrder);
+
+            // and reset the state of the DnD
+            setDragAndDrop({
+                ...dragAndDrop,
+                draggedFrom: null,
+                draggedTo: null,
+                isDragging: false
+            })
+    
+        }
+
+        const onDragLeave = () => {
+            setDragAndDrop({
+                ...dragAndDrop,
+                draggedTo: null
+            })
+        }
+
+
+
+        return (
+            <div className={`note-list container ${colorMode}`}>
+                {notesList.map((note, index) => 
                         <Note 
                             note={note} 
                             completeHandler={() => completeHandler(note)} 
                             deleteHandler={() => deleteHandler(note)}
-                            key={note.id} 
                             colorMode={colorMode}
+
+                            key={index}
+                            index={index}
+                            onDragStart={onDragStart}
+                            onDragOver={onDragOver}
+                            onDrop={onDrop}
+                            onDragLeave={onDragLeave}
+                            classNameDnD={`${dragAndDrop && dragAndDrop.draggedTo === Number(index) ? "drop-area" : ""}`}
                         />)
                 }
                 <div className={`notes-details ${colorMode}`}>
@@ -200,6 +252,20 @@ const NoteList = ({colorMode}) => {
                     <div className={`clear-completed ${colorMode}`} onClick={clearCompletedNotes}>Clear completed</div>
                 </div>
             </div>
+        )
+    }
+
+    /////////////////////////////////////////////////////////////////////////////////////
+
+    return ( 
+        <>
+            <NewNote 
+                handleChange={handleChange} 
+                handleSubmit= {handleSubmit}
+                newNote={newNote}
+                colorMode={colorMode}
+            />
+            {DragToReorderList()}
             {windowWidth < 480 && (
                 <div className="layout-mobile">
                     <NotesFilter 
